811a7fcd38e3f9d9a062bce21babe9d9266509bf
deanbunn <deanbunn@gmail.com>
2020-09-07 10:25:06 -0700

    20200907_1025hrs

---
+++ GCwP/gcwp_template.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import sys
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+class AppWindow(Gtk.ApplicationWindow):
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		
+
+		
+class Application(Gtk.Application):
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, application_id="org.bunn.gcwp", **kwargs)
+		self.window = None
+
+	def do_activate(self):
+		if not self.window:
+			self.window = AppWindow(application=self, title="GCwP Template")
+
+		self.window.show_all()
+		self.window.present()
+
+
+if __name__ == "__main__":
+	app = Application()
+	app.run(sys.argv)
+
+
